8ae9e8b6d7df379ce8355a6d5d43216254b5a415
hzoghi <hamed.zoghi@gmail.com>
2021-01-20 00:43:37 -0500

    implemented artists/search route function

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -377,7 +377,18 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
+  term = request.form.get('search_term', '')
+  search = '%{}%'.format(term)
+  artists = db.session.query(Artist.id, Artist.name).filter(Artist.name.ilike(search)).all()
+  _data = [artist._asdict() for artist in artists]
+  data = []
+  for _d in _data:
+    _d["num_upcoming_shows"] = db.session.query(Show).join(Artist).filter(Artist.id == _d['id'],Show.artist_id == Artist.id,
+    Show.start_time > datetime.now()).count()
+    data.append(_d)
+  response ={"count": len(data), "data": data}
+
+  mock_response={
     "count": 1,
     "data": [{
       "id": 4,
