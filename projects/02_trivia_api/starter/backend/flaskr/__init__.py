5bdbd94f87a1fc28a3feeb5e31a2a2962e31699e
hzoghi <hamed.zoghi@gmail.com>
2021-02-13 21:15:35 -0500

    added errorhandler functions for 404 and 422

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -231,6 +231,21 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      'success': False,
+      'error': 404,
+      'message': 'Resource not found'
+    }), 404
+  
+  @app.errorhandler(422)
+  def unprocessabe(error):
+    return jsonify({
+      'success': False,
+      'error': 422,
+      'message': 'Unprocessabe'
+    }), 422
   
   return app
 
