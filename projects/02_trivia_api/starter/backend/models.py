ca1e65ac2f3a424738173a54bd0b174348250d5a
hzoghi <hamed.zoghi@gmail.com>
2021-02-18 03:17:59 -0500

    implemented env variable support with dotenv

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -2,9 +2,10 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from dotenv import load_dotenv
 
-database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+load_dotenv()
+database_path = os.environ.get("SQLALCHEMY_DATABASE_URI_BASE")+os.environ.get('DB_NAME')
 
 db = SQLAlchemy()
 
@@ -14,7 +15,7 @@ setup_db(app)
 '''
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
     db.app = app
     db.init_app(app)
     db.create_all()
--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -2,10 +2,12 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
 from flaskr import create_app, QUESTIONS_PER_PAGE
 from models import setup_db, Question, Category
 
+load_dotenv()
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -15,7 +17,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = os.environ.get("SQLALCHEMY_DATABASE_URI_BASE") + self.database_name
         setup_db(self.app, self.database_path)
         self.quiz_category = {
             'previous_questions': [5, 9],
